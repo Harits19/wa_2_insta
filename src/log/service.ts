4513ee19df29e43d7a54cf585bd902af3fc79865
Abdullah Harits <harits.abdullah19@gmail.com>
2025-04-28 11:41:29 +0700

    better count logging

--- src/log/service.ts
+++ src/log/service.ts
@@ -1,6 +1,7 @@
 export default class LogService {
   static countTime(key: string) {
     let startTime: number | undefined;
+    key = `${key.trim()} `;
     const start = () => {
       startTime = Date.now();
       console.time(key);
@@ -16,10 +17,13 @@ export default class LogService {
       const seconds = Math.floor((elapsedTime % 60000) / 1000);
       const milliseconds = elapsedTime % 1000;
 
+      const addZero = (value: number, digit: number = 2) =>
+        value.toString().padStart(digit, "0");
       console.log(
-        `${key} ${minutes}:${seconds.toString().padStart(2, "0")}:${milliseconds
-          .toString()
-          .padStart(3, "0")}ms`
+        `${key}: ${minutes}:${addZero(seconds)}.${addZero(
+          milliseconds,
+          3
+        )} (m:ss.mmm)`
       );
     }, 3000);
 
